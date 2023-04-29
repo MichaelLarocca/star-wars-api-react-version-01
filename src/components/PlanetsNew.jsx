d71ee50970ca8f04db0501a9d6e5995258738218
Larocca <Larocca@RIVPRO1.net>
2023-04-29 13:10:50 -0400

    Working model!

--- src/components/PlanetsNew.jsx
+++ src/components/PlanetsNew.jsx
@@ -4,17 +4,19 @@ import axios from "axios";
 function PlanetsNew() {
   const [isLoading, setLoading] = useState(true); // Loading state
   const [starWarsData, setStarWarsData] = useState();
+  const [url, setUrl] = useState(`https://swapi.dev/api/planets/?page=1`)
 
   useEffect(() => { // useEffect hook
 
-   axios.get(`https://swapi.dev/api/planets/?page=1`)
+//    axios.get(`https://swapi.dev/api/planets/?page=1`)
+   axios.get(url)
     .then((response) => {
         // Get data
         setStarWarsData(response.data); //set  state
         setLoading(false); //set loading state
       });
 
-    }, []);
+    }, [url]);
 
     if (isLoading) {
         return (
@@ -32,12 +34,22 @@ function PlanetsNew() {
       return (
         <div>    
           <h1>SW Planets</h1>
-          <p>planetsPage: {starWarsData.count}</p>
-          <p>planetsPage: {starWarsData.next}</p>
-          <p>planetsPage: {starWarsData.previous}</p>
+          <p>planetsPage count: {starWarsData.count}</p>
+          <p>planetsPage next: {starWarsData.next}</p>
+          <p>planetsPage previous: {starWarsData.previous}</p>
           <p>{starWarsData.results[0].name}</p>
+          <button onClick={nextPlanetPage} disabled={starWarsData.next ? false : true}>Planet Page Next</button>
+          <button onClick={previousPage} disabled={starWarsData.previous ? false : true}>Planet Page Previous</button>
         </div>
       );    
+
+      function nextPlanetPage() {
+        setUrl(starWarsData.next);        
+      }
+
+      function previousPage() {
+        setUrl(starWarsData.previous); 
+      }
 }  
 
 export default PlanetsNew;
