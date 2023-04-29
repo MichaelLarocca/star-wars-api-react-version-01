a906832c49bfbe73f5a3d16e2b90aec1f6245ab8
Larocca <Larocca@RIVPRO1.net>
2023-04-29 10:30:42 -0400

    Kind of working. The problem is rendering before the fetching URLs are completed.

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,82 +1,58 @@
 import React, { useState, useEffect } from "react";
 
 function Planets() {
-    const [starWarsData, setStarWarsData] = useState({});
+    const [starWarsData, setStarWarsData] = useState("");
     const [planetsPage, setPlanetsPage] = useState(1);
-  
-    // useEffect(function() {
-    //     fetch(`https://swapi.dev/api/planets/?page=${planetsPage}`)
-    //         .then(res => res.json())
-    //         .then(data => setStarWarsData(data));
-    //             console.log(`starWarsData: ${starWarsData.next}`)
-    //           let count = starWarsData.count;
-    //             console.log(count);
-    //           let next = starWarsData.next;
-    //             console.log(`next page: ${next}`)   
-    //           let previous = starWarsData.previous;  
-    //             console.log(`previous ${previous}`)
-  
-    // }, [planetsPage]);
+    const [count, setCount] = useState(0);
+    const [next, setNext] = useState(0);
   
     useEffect(()=>{
       async function fetchPlanets() { 
-        let results = await fetch(`https://swapi.dev/api/planets/?page=${planetsPage}`);
-        const data = await results.json();
+
+        try {
+          const results = await fetch(`https://swapi.dev/api/planets/?page=${planetsPage}`);
+          const data = await results.json();
+    
+          setStarWarsData(data);
+
+          // let count = starWarsData.count;
+          setCount(starWarsData.count);
+            // console.log(`count: ${count}`);
+          // let next = starWarsData.next;
+          setNext(starWarsData.next);
+            // console.log(`next: ${next}`);
+
+          let previous = starWarsData.previous;
+          let planets = starWarsData.results;
   
-        setStarWarsData(data);
-          console.log(`starWarsData: ${starWarsData}`)
-  
-        let count = starWarsData.count;
-        let next = starWarsData.next;
-        //   console.log(`next page: ${next}`)
-        let previous = starWarsData.previous;
-        //   console.log(`count ${count}`);
-        //   console.log(`next ${next}`);
-        //   console.log(`previous ${previous}`)
-  
-        //   console.log(`planetsPage: ${planetsPage}`);
-  
-        let planets = starWarsData.results;
-        //   console.log(` starWarsData planets: ${planets}`);
-      } 
+         } catch (error) {
+        console.error(error);
+      }
+    }
       fetchPlanets();
     }, [planetsPage]);
   
     function handelClick() {
-      // if(next){
+      console.log(`planetsPage: ${planetsPage}`);
+      console.log(`next: ${next}`)
+      // if(next || !undefined || !null || planetsPage < 1) {
+      if(next && planetsPage < 7) {
         setPlanetsPage(prev => prev + 1);
-      // } else {
-        // setPlanetsPage(prev = 1);
-      // }
-      
+        // console.log(`next: ${next}`);
+      } else {
+        console.log(`All hand's on deck!`);
+        console.log(`next: ${next}`)
+        setPlanetsPage(prev => prev = 1);
+      }
+        
     }
   
     return (
       <>
         <div className='ctn'>
           <h1>SW API</h1>
-          <div>
-          {/* <button onClick={() => setPlanetsPage(prevCount => prevCount + 1)}>Next Page</button> */}
               <button onClick={handelClick}>Next Page</button>
-              {/* { <h2>The planetsPage is {planetsPage}</h2> */}
-              {/* {starWarsData && 
-                starWarsData.map((p) => {
-                  const {count, next, previous} = starWarsData;
-                  return (
-                    <div key={next}>
-                      <h5> {count} </h5>
-                      <h6> {previous }</h6>
-                    </div>
-                  )
-                })
-              } } */}
-              {/* { <p>starWarsData: {starWarsData.results[0].name}</p>  */}
-              {/* <pre>{JSON.stringify(starWarsData, null, 2)}</pre> */}
-              {starWarsData ? <pre>{JSON.stringify(starWarsData, null, 2)}</pre> : <p>No Data</p>}
-              {/* {starWarsData ? <p>starWarsData: {starWarsData.results[0].name}</p> : <p>No Data</p>}  */}
-
-          </div>
-          <p></p>
+              {starWarsData && <p>starWarsData: {starWarsData.results[0].name}</p>}
         </div>
       </>
     )
